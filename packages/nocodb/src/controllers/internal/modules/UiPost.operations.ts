bbfd380260958b3ab98ddb4e7f9c90a9c7c3d46e
DarkPhoenix2704 <anbarasun123@gmail.com>
2026-03-23 13:47:33 +0000

    chore: move to ee and feature gating

--- packages/nocodb/src/controllers/internal/modules/UiPost.operations.ts
+++ packages/nocodb/src/controllers/internal/modules/UiPost.operations.ts
@@ -140,8 +140,6 @@ export class UiPostOperations
     'listViewCreate' as const,
     'listViewUpdate' as const,
     'convertLinkToV2' as const,
-    'updateDateDependency' as const,
-    'deleteTableDateDependency' as const,
   ];
   httpMethod = 'POST' as const;
 
